65c47989d71fb2190ea51ce4b3d1ad16e34e274a
Matthieu <matthieunadeau96@gmail.com>
2018-02-20 15:00:25 -0500

    Making the Dice Functional

--- app/src/main/java/com/umatthieu/diceee/MainActivity.java
+++ app/src/main/java/com/umatthieu/diceee/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -17,13 +19,32 @@ public class MainActivity extends AppCompatActivity {
         Button rollbutton;
         rollbutton = (Button) findViewById(R.id.rollButton);
 
-        ImageView leftDice = (ImageView) findViewById(R.id.image_leftDice);
-        ImageView rightDice = (ImageView) findViewById(R.id.image_rightDice);
+        final ImageView leftDice = (ImageView) findViewById(R.id.image_leftDice);
+        final ImageView rightDice = (ImageView) findViewById(R.id.image_rightDice);
+
+        final int[] diceArray = {
+                R.drawable.dice1,
+                R.drawable.dice2,
+                R.drawable.dice3,
+                R.drawable.dice4,
+                R.drawable.dice5,
+                R.drawable.dice6
+        };
 
         rollbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d("Diceee","button has ben pressed!");
+
+                Random randomNumberGenerator = new Random();
+
+                int number = randomNumberGenerator.nextInt(6);
+                int number2 = randomNumberGenerator.nextInt(6);
+
+                Log.d("Diceee","The random number is: "+ number);
+
+                leftDice.setImageResource(diceArray[number]);
+                rightDice.setImageResource(diceArray[number2]);
             }
         });
     }
